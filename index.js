4ed5f6fddfa8b12f61dbec08ca23d7c6e6c2fb8b
stlgolfer <amigala@micds.org>
2018-12-25 22:16:12 -0600

    Add favicon

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 // render a nice webpage to turn it on and off
 app.get('/', (req, res) => {
